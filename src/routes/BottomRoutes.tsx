86abd2d84370a02ac11ab022d61107b3d58989e9
Erik Kauê <kaue_erik122@hotmail.com>
2025-08-25 20:35:54 -0300

    Bottom Tabs e Drawers - Customizando Bottom Navigator

--- src/routes/BottomRoutes.tsx
+++ src/routes/BottomRoutes.tsx
@@ -2,6 +2,7 @@ import {
   createBottomTabNavigator,
   BottomTabScreenProps,
 } from "@react-navigation/bottom-tabs";
+import { MaterialIcons } from "@expo/vector-icons";
 
 import { Product } from "@/app/Product";
 import { Home } from "@/app/Home";
@@ -20,10 +21,33 @@ export function BottomRoutes() {
   return (
     <Tab.Navigator
       initialRouteName="home"
-      screenOptions={{ headerShown: false }}
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#2c46b1",
+        tabBarInactiveTintColor: "#444444",
+        tabBarLabelPosition: "beside-icon",
+      }}
     >
-      <Tab.Screen name="home" component={Home} />
-      <Tab.Screen name="product" component={Product} />
+      <Tab.Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarLabel: "Início",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="product"
+        component={Product}
+        options={{
+          tabBarLabel: "Produto",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="shopping-cart" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
